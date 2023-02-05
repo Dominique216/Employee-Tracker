a6718e6b4bba339746cc2f0461255b528782ce36
Dominique Nix <dominiquenix@Dominiques-MacBook-Air.local>
2023-02-04 23:41:24 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 # UTA-bootcamp-challenge12
-This is challenge 12 for the UT Austin coding bootcamp.
+## Description 
+This application is an employee Tracker. It allows users to view and add departments, roles, and employees. It was created using node.js, inquirer, and mySQL. The user should be able to view a menu of opitions on the command line and view, edit, or update depending on the option they choose. 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+## Installation
+1. Copy the SSH key from the Repo into a folder on your own computer
+2. Once the files are installed, open the folder in your personal workspace
+3. For this application you will have to install inquirer, mysql2, and dotenv in the package.json file
+4. Add a .env file to the main foler and input your mySQL username, and password
+5. Open the integrated terminal on the schema.sql and run source schema.sql and then source seeds.sql
+6. Open the integrated terminal on the index.js file and run node index.js
+7. Follow the prompts to view or edit the employee data
+## Usage
+[Walkthrough Video](https://drive.google.com/file/d/10MbLwIEYhSZwEYhdjacqwG65IhHEvNwY/view)
+
+![Opening Integrated Terminal](./assets/sc1.png)
+![Running Appication](./assets/sc2.png)
+![Options](./assets/sc3.png)
+![After option is pressed](./assets/sc4.png)
+## License
+This project uses an MIT license. For more information click the license badge at the top of the README.
--- db/queries.sql
+++ db/queries.sql
@@ -37,56 +37,5 @@ UPDATE employee SET role_id  = 10 WHERE id = 7;
 SELECT name, SUM(salary) AS total_department_salary
 FROM role
 JOIN department ON department_id = department.id
-WHERE name = 'Sales';
+WHERE name = ? ;
 
-
--- SELECT m.manager_id 
--- FROM employee m
--- JOIN employee e ON e.last_name = m.id;
--- WITH employee AS (
---     SELECT last_name, manager_id
---     FROM employee
---     WHERE manager_id IS NULL
---     UNION ALL
-
---     SELECT e.last_name, e.manager_id
---     FROM employee e INNER JOIN employee m 
---     ON e.manager_id = m.id
--- )
-
--- SELECT * FROM employee ;
-
--- WITH RECURSIVE Emp_CTE (manager_id)
--- AS (
---     SELECT last_name,
---         manager_id, 
---     FROM employee
---     WHERE manager_id IS NULL
---     UNION ALL 
---         SELECT e.last_name,
---             e.manager_id, 
---         FROM employee e
---         INNER JOIN Emp_CTE ON Emp_CTE.id = e.manager_id
--- )
--- SELECT * FROM Emp_CTE;
-
--- SELECT m.id, m.last_name AS manager, e.manager_id
--- FROM employee e
--- INNER JOIN employee m
--- ON (m.id = e.manager_id);
-
--- SELECT last_name, manager_id FROM employee;
-
--- SELECT employee.id, employee.first_name, employee.last_name, role.title, name AS department, role.salary, employee.manager_id
--- FROM department
--- JOIN role ON department_id = department.id
--- JOIN employee ON role_id = role.id;
-
-
-
-
--- SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, m.id, m.last_name
--- FROM employee e
--- JOIN employee m ON (e.manager_id = m.id)
--- JOIN role ON role_id = role.id
--- JOIN department ON department_id = ; 
